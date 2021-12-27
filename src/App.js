04a4397736257a367bc64b6efad00dab598de458
Aman Bhanse <amanbhanse22@gmail.com>
2021-12-27 18:18:18 +0530

    Add Routing and homepage component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import HomePage from "./Pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <>
+            <h2>Welcome to my blog</h2>
+            <p>
+                {" "}
+                lsfsdfsdfadbfakjdbsfkjadsf lsfsdfsdfadbfakjdbsfkjadsf lsfsdfsdfadbfakjdbsfkjadsf
+                lsfsdfsdfadbfakjdbsfkjadsf lsfsdfsdfadbfakjdbsfkjadsf lsfsdfsdfadbfakjdbsfkjadsf
+                lsfsdfsdfadbfakjdbsfkjadsf lsfsdfsdfadbfakjdbsfkjadsf lsfsdfsdfadbfakjdbsfkjadsf
+                lsfsdfsdfadbfakjdbsfkjadsf lsfsdfsdfadbfakjdbsfkjadsf lsfsdfsdfadbfakjdbsfkjadsf
+                lsfsdfsdfadbfakjdbsfkjadsf lsfsdfsdfadbfakjdbsfkjadsf{" "}
+            </p>
+        </>
+    );
+}
+
+export default HomePage;
