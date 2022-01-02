93798e77ac17d65ffafccdf07cd1e7351cb5a307
Aman Bhanse <amanbhanse22@gmail.com>
2022-01-02 20:01:43 +0530

    Add fetch to pages

--- src/Pages/ArticlePage.js
+++ src/Pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articlesContent from "./ArticlesContent";
 import NotFoundPage from "./NotFoundPage";
@@ -8,10 +8,22 @@ function ArticlePage() {
 
     const article = articlesContent.find((article) => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0 });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        };
+        fetchData();
+    }, [name]);
+
     if (!article) return <NotFoundPage />;
     return (
         <>
             <h2>{article.title}</h2>
+            <p>This article has {articleInfo.upvotes} votes</p>
             {article.content.map((paragraph, key) => {
                 return <p key={key}>{paragraph}</p>;
             })}
